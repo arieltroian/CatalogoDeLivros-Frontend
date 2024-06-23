01a43f1a4ce9aeb5bc93f91c5814e51948eef2bb
arieltroian <arieltroian@outlook.com>
2024-06-22 21:22:06 -0300

    feat: add initial project setup configurations

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Catálogo de Livros</h1>
+      <div className="card-grid"></div>
+    </div>
+  );
+}
+
+export default App;
