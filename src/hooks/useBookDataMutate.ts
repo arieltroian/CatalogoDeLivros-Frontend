5d97a63eac6425f4c93d4460aae0b7b2032b87b1
arieltroian <arieltroian@outlook.com>
2024-06-26 11:05:13 -0300

    feat: add update and delete operations for books

--- src/hooks/useBookDataMutate.ts
+++ src/hooks/useBookDataMutate.ts
@@ -9,10 +9,44 @@ const postData = async (data: BookData): AxiosPromise<any> => {
   return response;
 };
 
+const updateData = async (data: BookData): AxiosPromise<any> => {
+  const response = axios.put(`${API_URL}/book/${data.id}`, data);
+  return response;
+};
+
+const deleteData = async (id: number): AxiosPromise<any> => {
+  const response = axios.delete(`${API_URL}/book/${id}`);
+  return response;
+};
+
 export function useBookDataMutate() {
   const queryClient = useQueryClient();
   const mutate = useMutation({
     mutationFn: postData,
+    retry: 2,
+    onSuccess: () => {
+      queryClient.invalidateQueries(["book-data"]);
+    },
+  });
+  return mutate;
+}
+
+export function useBookDataUpdate() {
+  const queryClient = useQueryClient();
+  const mutate = useMutation({
+    mutationFn: updateData,
+    retry: 2,
+    onSuccess: () => {
+      queryClient.invalidateQueries(["book-data"]);
+    },
+  });
+  return mutate;
+}
+
+export function useBookDataDelete() {
+  const queryClient = useQueryClient();
+  const mutate = useMutation({
+    mutationFn: deleteData,
     retry: 2,
     onSuccess: () => {
       queryClient.invalidateQueries(["book-data"]);
